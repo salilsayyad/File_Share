826536543abf61c07fe76ce5acd11fd627a318f2
Salil <1999salil2016@gmail.com>
2023-05-30 13:15:08 +0530

    m5

--- Server/database/db.js
+++ Server/database/db.js
@@ -7,9 +7,9 @@ const DBConnection = async () => {
     const USERNAME = process.env.DB_USERNAME;
     const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-1vikj18-shard-00-00.xtawrkg.mongodb.net:27017,ac-1vikj18-shard-00-01.xtawrkg.mongodb.net:27017,ac-1vikj18-shard-00-02.xtawrkg.mongodb.net:27017/?ssl=true&replicaSet=atlas-13fy2k-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const MONGO_URI = "mongodb+srv://user:Delta@123456789@cluster0.wwxcdie.mongodb.net/?retryWrites=true&w=majority";
     try {
-        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        await mongoose.connect("mongodb+srv://user:salil@cluster0.wwxcdie.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
         console.log('Database connected successfully');
     } catch (error) {
         console.log('Error while connecting with the database ', error.message);
